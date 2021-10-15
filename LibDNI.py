fde81aa0945947ae6403350a5af92c370f42be9d
Bl4nc018 <92156488+Bl4nc018@users.noreply.github.com>
2021-10-15 12:34:31 +0200

    Update LibDNI.py

--- LibDNI.py
+++ LibDNI.py
@@ -1,8 +1,5 @@
 #archivo LibDNI.py
-def calculoLetra(numerodni):
-  letras=("TRWAGMYFPDXBNJZSQVHLCKE")
-  dni=int(input("Introduzca los números de su DNI aquí porfavor: "))
-  numero = numerodni%23
-  numeroL = numero(dni)
-  letra = letras[numeroL]
+def calculoLetra(dni):
+  letras = "TRWAGMYFPDXBNJZSQVHLCKE"
+  letra = letras[dni%23]
   return letra
